3a51245fe8c9088f9a250e360c87c0c66eef1237
jonasrothfuss <jonas.rothfuss@gmx.de>
2017-12-19 14:39:57 +0100

    economically inspired densities + histogram plots of pdfs

---
+++ density_simulation/econ_densities.py
@@ -0,0 +1,50 @@
+import scipy.stats as stats
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def econ_conditional_pdf_1_sample(x, n_samples = 1000):
+  y = x ** 2 + np.random.normal(loc=0, scale=0.5, size=[n_samples])
+  return y
+
+
+def econ_pdf_1_sample(n_samples = 1000):
+  x = np.abs(np.random.standard_normal(size=[n_samples]))
+  y = x**2 + np.random.normal(loc=0, scale=0.5, size=[n_samples])
+  return np.stack([x,y], axis=1)
+
+
+def plot_histogram_pdf_econ1(n_samples=100000):
+  sim_data = econ_pdf_1_sample(n_samples=n_samples)
+  print(sim_data.shape)
+
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  x, y = sim_data[:, 0], sim_data[:, 1]
+  hist, xedges, yedges = np.histogram2d(x, y, bins=50, range=[[0, 2], [0, 2]])
+
+  xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25)
+  xpos = xpos.flatten('F')
+  ypos = ypos.flatten('F')
+  zpos = np.zeros_like(xpos)
+
+  # Construct arrays with the dimensions for the 16 bars.
+  dx = 0.5 * np.ones_like(zpos)
+  dy = dx.copy()
+  dz = hist.flatten()
+
+  ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color='b', zsort='average')
+  plt.show()
+
+def plot_histogram_conditional_pdf_econ1(x, n_samples = 100000):
+  sim_data = econ_conditional_pdf_1_sample(x, n_samples=n_samples)
+  plt.hist(sim_data, bins=100)
+  plt.show()
+
+def main():
+  plot_histogram_conditional_pdf_econ1(x=1)
+
+
+
+if __name__ == "__main__":
+  main()
