d4362fa4c4b3f273a28739f3b94f605822eddf9d
jonasrothfuss <jonas.rothfuss@gmx.de>
2017-12-21 12:22:47 +0100

    add unit tests for sampling kernel center points

---
+++ tests/density_estimator_tests.py
@@ -0,0 +1,35 @@
+from density_estimator.helpers import *
+import unittest
+
+class TestHelpers(unittest.TestCase):
+
+  def test_shape_center_point(self):
+    methods = ["all", "random", "k_means" , "agglomerative"]
+    for m in methods:
+      Y = np.random.uniform(size=(120,2))
+      centers = sample_center_points(Y, method=m, k=50)
+      self.assertEquals(centers.ndim, Y.ndim)
+      self.assertEquals(centers.shape[1], Y.shape[1])
+
+  def test_shape_center_point_k_means(self):
+    Y = np.asarray([1.0, 2.0])
+    centers = sample_center_points(Y, method="k_means", k=1)
+    self.assertAlmostEqual(Y.mean(), centers.mean())
+
+  def test_shape_center_point_agglomerative(self):
+    Y = np.random.uniform(size=[20,3])
+    centers = sample_center_points(Y, method="agglomerative", k=1)
+    self.assertAlmostEqual(Y.mean(), centers.mean())
+
+  def test_shape_center_point_keep_edges(self):
+    methods = ["random", "k_means", "agglomerative"]
+    for m in methods:
+      Y = np.random.uniform(size=(10,2))
+      centers = sample_center_points(Y, method=m, k=3, keep_edges=True)
+      self.assertEquals(centers.ndim, Y.ndim)
+      self.assertEquals(centers.shape[1], Y.shape[1])
+
+
+
+if __name__ == '__main__':
+  unittest.main()
