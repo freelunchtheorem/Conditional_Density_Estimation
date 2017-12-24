453531c04c60025bcea8bab9314e08e2212ab9d8
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2017-12-24 14:33:54 +0100

    implemented plot for LQ cond. dens. model with toy_dataset2, kmn prdensity prediction call simplified, increased n_centers of KMN for evaluation

--- evaluation/sim_eval.py
+++ evaluation/sim_eval.py
@@ -2,18 +2,69 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from density_estimator.kmn import KernelMixtureNetwork
+from density_estimator.LDRE import LQConditionalDensityEstimation
 from matplotlib.lines import Line2D
 import pandas as pd
 from density_simulation.econ_densities import build_econ1_dataset
-from density_simulation.toy_densities import build_toy_dataset, bild_toy_dataset2
+from density_simulation.toy_densities import build_toy_dataset, build_toy_dataset2
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+
+def plot_fitted_distribution():
+  n_observations = 2000  # number of data points
+  n_features = 3  # number of features
+
+  np.random.seed(22)
+
+  X_train, X_test, Y_train, Y_test = build_toy_dataset2(n_observations)
+  model = LQConditionalDensityEstimation(center_sampling_method='random', bandwidth=1, n_centers=1)
+
+  X_train = np.random.normal(loc=0, size=[n_observations, 1])
+  Y_train = 3 * X_train + np.random.normal(loc=0, size=[n_observations, 1])
+  X_test = np.random.normal(loc=0, size=[400, 1])
+  Y_test = 3 * X_test + np.random.normal(loc=0, size=[400, 1])
+
+  model.fit(X_train, Y_train)
+
+
+  fig, ax = plt.subplots()
+  fig.set_size_inches(10, 8)
+  sns.regplot(X_train, Y_train, fit_reg=False)
+  plt.show()
+
+
+  n_samples = 2000
+  X_plot = np.expand_dims(np.asarray([-4 for _ in range(n_samples)]), axis=1)
+  Y_plot = np.linspace(-10, 10, num=n_samples)
+  result = model.predict(X_plot, Y_plot)
+  plt.plot(Y_plot, result)
+  plt.show()
+
+  n_samples = 500
+  linspace_x = np.linspace(-15, 15, num=n_samples)
+  linspace_y = np.linspace(-15, 15, num=n_samples)
+  X, Y = np.meshgrid(linspace_x, linspace_y)
+  X, Y = X.flatten(), Y.flatten()
+
+  print(X,Y, Y)
+  Z = model.predict(X, Y)
+
+  X, Y, Z = X.reshape([n_samples, n_samples]), Y.reshape([n_samples, n_samples]), Z.reshape([n_samples, n_samples])
+  fig = plt.figure()
+  ax = fig.gca(projection='3d')
+  surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                         linewidth=0, antialiased=True)
+
+  plt.show()
 
 
 
-def main():
+def eval1():
   n_observations = 2000  # number of data points
   n_features = 1  # number of features
 
-  X_train, X_test, y_train, y_test = bild_toy_dataset2(n_observations)
+  X_train, X_test, y_train, y_test = build_econ1_dataset(n_observations)
   print("Size of features in training data: {}".format(X_train.shape))
   print("Size of output in training data: {}".format(y_train.shape))
   print("Size of features in test data: {}".format(X_test.shape))
@@ -22,28 +73,36 @@ def main():
   fig, ax = plt.subplots()
   fig.set_size_inches(10, 8)
   sns.regplot(X_train, y_train, fit_reg=False)
-  plt.savefig('toydata.png')
-  plt.show()
+  # plt.savefig('toydata.png')
+  # plt.show()
   # plot.figure.size = 100
   # plt.show()
 
-  kmn = KernelMixtureNetwork(train_scales=True, n_centers=10)
+  kmn = KernelMixtureNetwork(train_scales=True, n_centers=20)
   kmn.fit(X_train, y_train, n_epoch=300, eval_set=(X_test, y_test))
   kmn.plot_loss()
-  plt.savefig('trainplot.png')
-
+  # plt.savefig('trainplot.png')
   samples = kmn.sample(X_test)
+  print(X_test.shape, samples.shape)
   jp = sns.jointplot(X_test.ravel(), samples, kind="hex", stat_func=None, size=10)
   jp.ax_joint.add_line(Line2D([X_test[0][0], X_test[0][0]], [-40, 40], linewidth=3))
   jp.ax_joint.add_line(Line2D([X_test[1][0], X_test[1][0]], [-40, 40], color='g', linewidth=3))
   jp.ax_joint.add_line(Line2D([X_test[2][0], X_test[2][0]], [-40, 40], color='r', linewidth=3))
   plt.savefig('hexplot.png')
   plt.show()
-  d = kmn.predict_density(np.expand_dims(np.asarray([0.5,1,2]), axis=1).reshape(-1, 1), resolution=1000)
+  d = kmn.predict_density(X_test[0:3, :].reshape(-1, 1), resolution=1000)
   df = pd.DataFrame(d).transpose()
   df.index = np.linspace(kmn.y_min, kmn.y_max, num=1000)
   df.plot(legend=False, linewidth=3, figsize=(12.2, 8))
   plt.savefig('conditional_density.png')
+
+def main():
+  plot_fitted_distribution()
+
+
+
+
+
 
 if __name__ == "__main__":
   main()
