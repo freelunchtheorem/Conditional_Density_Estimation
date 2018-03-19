b98ce658940b2a8b580b5358abfc488e318b7721
jonasrothfuss <jonas.rothfuss@gmx.de>
2018-03-19 13:08:49 +0100

    Wrapped statsmodel conditional KDE in BaseDensityEstimator class

--- cde/density_estimator/__init__.py
+++ cde/density_estimator/__init__.py
@@ -2,3 +2,4 @@ from .KMN import KernelMixtureNetwork
 from .LSCDE import LSConditionalDensityEstimation
 from .NKDE import NeighborKernelDensityEstimation
 from .base import BaseDensityEstimator
+from .CKDE import ConditionalKernelDensityEstimation
