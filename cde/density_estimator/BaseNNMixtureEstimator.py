bugfix covariance computation in MixtureNN + making data normalization layer serializable

cde/density_estimator/BaseNNMixtureEstimator.py
@@ -50,7 +50,7 @@ class BaseNNMixtureEstimator(LayersPowered, Serializable, BaseDensityEstimator):
     weights, locs, scales = self._get_mixture_components(x_cond)
 
     for i in range(x_cond.shape[0]):
-      c1 = np.diag(weights[i].dot(scales[i]))
+      c1 = np.diag(weights[i].dot(scales[i]**2))
 
       c2 = np.zeros(c1.shape)
       for j in range(weights.shape[1]):

cde/density_estimator/KMN.py
@@ -102,7 +102,7 @@ class KernelMixtureNetwork(BaseNNMixtureEstimator): #TODO: KMN doesn not anymore
     self._build_model()
 
     # initialize LayersPowered --> provides functions for serializing tf models
-    LayersPowered.__init__(self, [self.core_output_layer, self.locs_layer, self.scales_layer])
+    LayersPowered.__init__(self, [self.core_output_layer, self.locs_layer, self.scales_layer, self.layer_in_y])
 
   def fit(self, X, Y, random_seed=None, verbose=True, **kwargs):
     """ Fits the conditional density model with provided data
@@ -174,10 +174,10 @@ class KernelMixtureNetwork(BaseNNMixtureEstimator): #TODO: KMN doesn not anymore
     implementation of the KMN
     """
     with tf.variable_scope(self.name):
-      layer_in_x, layer_in_y = self._build_input_layers() # add playeholders, data_normalization and data_noise if desired
+      self.layer_in_x, self.layer_in_y = self._build_input_layers() # add playeholders, data_normalization and data_noise if desired
 
-      self.X_in = L.get_output(layer_in_x)
-      self.Y_in = L.get_output(layer_in_y)
+      self.X_in = L.get_output(self.layer_in_x)
+      self.Y_in = L.get_output(self.layer_in_y)
 
       # get batch size
       self.batch_size = tf.shape(self.X_ph)[0]
@@ -185,7 +185,7 @@ class KernelMixtureNetwork(BaseNNMixtureEstimator): #TODO: KMN doesn not anymore
       # create core multi-layer perceptron
       core_network = MLP(
         name="core_network",
-        input_layer=layer_in_x,
+        input_layer=self.layer_in_x,
         output_dim=self.n_centers*self.n_scales,
         hidden_sizes=self.hidden_sizes,
         hidden_nonlinearity=self.hidden_nonlinearity,
@@ -217,7 +217,7 @@ class KernelMixtureNetwork(BaseNNMixtureEstimator): #TODO: KMN doesn not anymore
       assert len(self.scales_array) == self.n_scales
 
       # put mixture components together
-      self.y_input = L.get_output(layer_in_y)
+      self.y_input = L.get_output(self.layer_in_y)
       self.cat = cat = Categorical(logits=self.logits)
       self.components = components = [MultivariateNormalDiag(loc=loc, scale_diag=scale) for loc in self.locs_array for scale in scales_array]
       self.mixture = mixture = Mixture(cat=cat, components=components)
@@ -230,7 +230,7 @@ class KernelMixtureNetwork(BaseNNMixtureEstimator): #TODO: KMN doesn not anymore
 
       # tensor to compute probabilities
       if self.data_normalization:
-        self.pdf_ = mixture.prob(self.y_input) / self.std_y_sym
+        self.pdf_ = mixture.prob(self.y_input) / tf.reduce_prod(self.std_y_sym)
       else:
         self.pdf_ = mixture.prob(self.y_input)
 
@@ -258,15 +258,13 @@ class KernelMixtureNetwork(BaseNNMixtureEstimator): #TODO: KMN doesn not anymore
 
     locs, weights, scales = self.sess.run([self.locs_unnormalized, self.weights, self.scales_unnormalized], feed_dict={self.X_ph: X})
 
-    locs = np.tile(locs.reshape([1] + list(locs.shape)), (X.shape[0], scales.shape[0],1))
-
-    cov = np.concatenate([scales[i,:].reshape((1,1,self.ndim_y)) for i in range(scales.shape[0]) for j in range(self.n_centers)], axis=1)
-    cov = np.tile(cov, (X.shape[0], 1, 1))
+    locs = np.concatenate([np.tile(np.expand_dims(locs[i:i+1], axis=1), (X.shape[0], self.n_scales, 1)) for i in range(self.n_centers)], axis=1)
+    cov = np.tile(np.expand_dims(scales, axis=0), (X.shape[0], self.n_centers, 1))
 
     assert weights.shape[0] == locs.shape[0] == cov.shape[0] == X.shape[0]
     assert weights.shape[1] == locs.shape[1] == cov.shape[1] == self.n_centers*self.n_scales
     assert locs.shape[2] == cov.shape[2] == self.ndim_y
-    assert locs.ndim == 3 and cov.ndim == 3 and weights.ndim == 3
+    assert locs.ndim == 3 and cov.ndim == 3 and weights.ndim == 2
     return weights, locs, cov
 
   def __str__(self):

cde/density_estimator/MDN.py
@@ -74,7 +74,7 @@ class MixtureDensityNetwork(BaseNNMixtureEstimator):
     self._build_model()
 
     # initialize LayersPowered --> provides functions for serializing tf models
-    LayersPowered.__init__(self, [self.softmax_layer_weights, self.softplus_layer_scales, self.reshape_layer_locs])
+    LayersPowered.__init__(self, [self.softmax_layer_weights, self.softplus_layer_scales, self.reshape_layer_locs, self.layer_in_y])
 
   def fit(self, X, Y, random_seed=None, verbose=True, eval_set=None, **kwargs):
     """ Fits the conditional density model with provided data
@@ -145,13 +145,12 @@ class MixtureDensityNetwork(BaseNNMixtureEstimator):
     """
 
     with tf.variable_scope(self.name):
-      layer_in_x, layer_in_y = self._build_input_layers()  # add playeholders, data_normalization and data_noise if desired
-
+      self.layer_in_x, self.layer_in_y = self._build_input_layers()  # add playeholders, data_normalization and data_noise if desired
       # create core multi-layer perceptron
       mlp_output_dim = 2 * self.ndim_y * self.n_centers + self.n_centers
       core_network = MLP(
               name="core_network",
-              input_layer=layer_in_x,
+              input_layer=self.layer_in_x,
               output_dim=mlp_output_dim,
               hidden_sizes=self.hidden_sizes,
               hidden_nonlinearity=self.hidden_nonlinearity,
@@ -181,7 +180,7 @@ class MixtureDensityNetwork(BaseNNMixtureEstimator):
       self.weights = L.get_output(self.softmax_layer_weights)
 
       # # put mixture components together
-      self.y_input = L.get_output(layer_in_y)
+      self.y_input = L.get_output(self.layer_in_y)
       self.cat = cat = Categorical(logits=self.logits)
       self.components = components = [MultivariateNormalDiag(loc=loc, scale_diag=scale) for loc, scale
                      in zip(tf.unstack(self.locs, axis=1), tf.unstack( self.scales, axis=1))]
@@ -198,7 +197,7 @@ class MixtureDensityNetwork(BaseNNMixtureEstimator):
 
       # tensor to compute probabilities
       if self.data_normalization:
-        self.pdf_ = mixture.prob(self.y_input) / self.std_y_sym
+        self.pdf_ = mixture.prob(self.y_input) / tf.reduce_prod(self.std_y_sym)
       else:
         self.pdf_ = mixture.prob(self.y_input)
 
@@ -221,4 +220,8 @@ class MixtureDensityNetwork(BaseNNMixtureEstimator):
   def _get_mixture_components(self, X):
     assert self.fitted
     weights, locs, scales = self.sess.run([self.weights, self.locs_unnormalized, self.scales_unnormalized], feed_dict={self.X_ph: X})
+    assert weights.shape[0] == locs.shape[0] == scales.shape[0] == X.shape[0]
+    assert weights.shape[1] == locs.shape[1] == scales.shape[1] == self.n_centers
+    assert locs.shape[2] == scales.shape[2] == self.ndim_y
+    assert locs.ndim == 3 and scales.ndim == 3 and weights.ndim == 2
     return weights, locs, scales

cde/tests/unittests_estimators.py
@@ -200,7 +200,8 @@ class TestRiskMeasures(unittest.TestCase):
     from tensorflow import set_random_seed
     set_random_seed(22)
 
-    data = np.random.normal([2, 2, 7, -2], 5, size=(5000, 4))
+    scale = 5.0
+    data = np.random.normal(loc=[2, 2, 7, -2], scale=scale, size=(5000, 4))
     X = data[:, 0:2]
     Y = data[:, 2:4]
 
@@ -208,8 +209,9 @@ class TestRiskMeasures(unittest.TestCase):
     model.fit(X, Y)
 
     cov_est = model.covariance(x_cond=np.array([[0, 1]]))
+    print(cov_est)
     self.assertAlmostEqual(cov_est[0][1][0], 0.0, places=1)
-    self.assertLessEqual(cov_est[0][0][0] - 5.0,1.0)
+    self.assertLessEqual(np.abs(np.sqrt(cov_est[0][0][0]) - scale),1.0)
 
   def test_conditional_value_at_risk_mixture(self):
     np.random.seed(24)
@@ -249,9 +251,9 @@ class TestRiskMeasures(unittest.TestCase):
 
 class TestConditionalDensityEstimators_2d_gaussian(unittest.TestCase):
 
-  def get_samples(self, std=1.0):
+  def get_samples(self, mu=2, std=1.0):
     np.random.seed(22)
-    data = np.random.normal([2, 2], std, size=(2000, 2))
+    data = np.random.normal([mu, mu], std, size=(2000, 2))
     X = data[:, 0]
     Y = data[:, 1]
     return X, Y
@@ -282,45 +284,79 @@ class TestConditionalDensityEstimators_2d_gaussian(unittest.TestCase):
       self.assertLessEqual(np.mean(np.abs(p_true - p_est)), 0.1)
 
   def test_KMN_with_2d_gaussian(self):
-    X, Y = self.get_samples()
+    mu = -2.0
+    std = 2.0
+    X, Y = self.get_samples(mu=mu, std=std)
 
     for method in ["agglomerative"]:
       with tf.Session() as sess:
-        model = KernelMixtureNetwork("kmn_"+method, 1, 1, center_sampling_method=method, n_centers=100)
+        model = KernelMixtureNetwork("kmn_"+method, 1, 1, center_sampling_method=method, n_centers=20,
+                                     hidden_sizes=(16, 16), init_scales=np.array([0.5]), train_scales=True,
+                                     data_normalization=False)
         model.fit(X, Y)
 
-        y = np.arange(-1, 5, 0.5)
-        x = np.asarray([2 for i in range(y.shape[0])])
+        y = np.arange(mu - 3 * std, mu + 3 * std, 6 * std / 20)
+        x = np.asarray([mu for i in range(y.shape[0])])
         p_est = model.pdf(x, y)
-        p_true = norm.pdf(y, loc=2, scale=1)
+        p_true = norm.pdf(y, loc=mu, scale=std)
         self.assertLessEqual(np.mean(np.abs(p_true - p_est)), 0.1)
 
         p_est = model.cdf(x, y)
-        p_true = norm.cdf(y, loc=2, scale=1)
+        p_true = norm.cdf(y, loc=mu, scale=std)
+        self.assertLessEqual(np.mean(np.abs(p_true - p_est)), 0.1)
+
+  def test_KMN_with_2d_gaussian_2(self):
+    mu = 200
+    std = 23
+    X, Y = self.get_samples(mu=mu, std=std)
+
+    for method in ["agglomerative"]:
+      with tf.Session() as sess:
+        model = KernelMixtureNetwork("kmn2_" + method, 1, 1, center_sampling_method=method, n_centers=10,
+                                     hidden_sizes=(16, 16), init_scales=np.array([1.0]), train_scales=True,
+                                     data_normalization=True)
+        model.fit(X, Y)
+
+        y = np.arange(mu - 3 * std, mu + 3 * std, 6 * std / 20)
+        x = np.asarray([mu for i in range(y.shape[0])])
+        p_est = model.pdf(x, y)
+        p_true = norm.pdf(y, loc=mu, scale=std)
+        self.assertLessEqual(np.mean(np.abs(p_true - p_est)), 0.1)
+
+        p_est = model.cdf(x, y)
+        p_true = norm.cdf(y, loc=mu, scale=std)
         self.assertLessEqual(np.mean(np.abs(p_true - p_est)), 0.1)
 
   def test_KMN_with_2d_gaussian_sampling(self):
-    np.random.seed(23)
-    X, Y = self.get_samples()
+    np.random.seed(22)
+    X, Y = self.get_samples(mu=5)
 
-    model = KernelMixtureNetwork("kmn_sampling", 1, 1, n_centers=5)
+    import time
+    t = time.time()
+    model = KernelMixtureNetwork("kmn_sampling", 1, 1, center_sampling_method='k_means', n_centers=5,
+                                 n_training_epochs=200, data_normalization=False)
+    print("time to build model:", time.time() - t)
+    t = time.time()
+
     model.fit(X, Y)
+    print("time to fit model:", time.time() - t)
 
-    x_cond = np.ones(shape=(200000,1))
+    x_cond = 2 * np.ones(shape=(2000000,1))
     _, y_sample = model.sample(x_cond)
-    self.assertAlmostEqual(np.mean(y_sample), float(model.mean_(y_sample[1])), places=1)
-    self.assertAlmostEqual(np.std(y_sample), float(model.covariance(y_sample[1])), places=1)
+    print(np.mean(y_sample), np.std(y_sample))
+    self.assertAlmostEqual(np.mean(y_sample), float(model.mean_(x_cond[1])), places=1)
+    self.assertAlmostEqual(np.std(y_sample), float(model.covariance(x_cond[1])), places=1)
 
-    x_cond = np.ones(shape=(200000, 1))
+    x_cond = np.ones(shape=(400000, 1))
     x_cond[0,0] = 5.0
     _, y_sample = model.sample(x_cond)
-    self.assertAlmostEqual(np.mean(y_sample), float(model.mean_(y_sample[1])), places=1)
-    self.assertAlmostEqual(np.std(y_sample), float(model.covariance(y_sample[1])), places=1)
+    self.assertAlmostEqual(np.mean(y_sample), float(model.mean_(x_cond[1])), places=1)
+    self.assertAlmostEqual(np.std(y_sample), float(model.covariance(x_cond[1])), places=1)
 
   def test_MDN_with_2d_gaussian_sampling(self):
     X, Y = self.get_samples()
 
-    model = MixtureDensityNetwork("mdn_gaussian_sampling", 1, 1, n_centers=5)
+    model = MixtureDensityNetwork("mdn_gaussian_sampling", 1, 1, n_centers=5, n_training_epochs=200)
     model.fit(X, Y)
 
     x_cond = np.ones(shape=(10**6,1))
@@ -329,19 +365,39 @@ class TestConditionalDensityEstimators_2d_gaussian(unittest.TestCase):
     self.assertAlmostEqual(np.std(y_sample), float(model.covariance(y_sample[1])), places=0)
 
   def test_MDN_with_2d_gaussian(self):
-    X, Y = self.get_samples()
+    mu = 200
+    std = 23
+    X, Y = self.get_samples(mu=mu, std=std)
 
-    model = MixtureDensityNetwork("mdn", 1, 1, n_centers=5)
+    model = MixtureDensityNetwork("mdn", 1, 1, n_centers=10, data_normalization=True)
     model.fit(X, Y)
 
-    y = np.arange(-1, 5, 0.5)
-    x = np.asarray([2 for i in range(y.shape[0])])
+    y = np.arange(mu - 3 * std, mu + 3 * std, 6 * std / 20)
+    x = np.asarray([mu for i in range(y.shape[0])])
     p_est = model.pdf(x, y)
-    p_true = norm.pdf(y, loc=2, scale=1)
+    p_true = norm.pdf(y, loc=mu, scale=std)
     self.assertLessEqual(np.mean(np.abs(p_true - p_est)), 0.1)
 
     p_est = model.cdf(x, y)
-    p_true = norm.cdf(y, loc=2, scale=1)
+    p_true = norm.cdf(y, loc=mu, scale=std)
+    self.assertLessEqual(np.mean(np.abs(p_true - p_est)), 0.1)
+
+  def test_MDN_with_2d_gaussian2(self):
+    mu = -5
+    std = 2.5
+    X, Y = self.get_samples(mu=mu, std=std)
+
+    model = MixtureDensityNetwork("mdn2", 1, 1, n_centers=5, weight_normalization=True)
+    model.fit(X, Y)
+
+    y = np.arange(mu - 3 * std, mu + 3 * std, 6 * std / 20)
+    x = np.asarray([mu for i in range(y.shape[0])])
+    p_est = model.pdf(x, y)
+    p_true = norm.pdf(y, loc=mu, scale=std)
+    self.assertLessEqual(np.mean(np.abs(p_true - p_est)), 0.1)
+
+    p_est = model.cdf(x, y)
+    p_true = norm.cdf(y, loc=mu, scale=std)
     self.assertLessEqual(np.mean(np.abs(p_true - p_est)), 0.1)
 
   def test_CDE_with_2d_gaussian(self):
@@ -372,7 +428,7 @@ class TestSerializationDensityEstimators(unittest.TestCase):
   def testPickleUnpickleMDN(self):
     X, Y = self.get_samples()
     with tf.Session() as sess:
-      model = MixtureDensityNetwork("mdn_pickle", 2, 2, n_training_epochs=10)
+      model = MixtureDensityNetwork("mdn_pickle", 2, 2, n_training_epochs=10, data_normalization=True,  weight_normalization=False)
       model.fit(X, Y)
       pdf_before = model.pdf(X, Y)
 
@@ -389,7 +445,7 @@ class TestSerializationDensityEstimators(unittest.TestCase):
   def testPickleUnpickleKDN(self):
     X, Y = self.get_samples()
     with tf.Session() as sess:
-      model = KernelMixtureNetwork("kde", 2, 2, n_centers=10, n_training_epochs=10)
+      model = KernelMixtureNetwork("kde", 2, 2, n_centers=10, n_training_epochs=10, data_normalization=True,  weight_normalization=True)
       model.fit(X, Y)
       pdf_before = model.pdf(X, Y)
 
@@ -573,25 +629,27 @@ class TestRegularization(unittest.TestCase):
 
       cond_cov = np.mean(model.covariance(Y), axis=0)
       print(cond_cov)
-      self.assertGreaterEqual(cond_cov[0][0], std * 0.7)
-      self.assertLessEqual(cond_cov[0][0], std * 1.3)
-      self.assertGreaterEqual(cond_cov[1][1], std * 0.7)
-      self.assertLessEqual(cond_cov[1][1], std * 1.3)
+      self.assertGreaterEqual(cond_cov[0][0], std**2 * 0.7)
+      self.assertLessEqual(cond_cov[0][0], std**2 * 1.3)
+      self.assertGreaterEqual(cond_cov[1][1], std**2 * 0.7)
+      self.assertLessEqual(cond_cov[1][1], std**2 * 1.3)
 
   def test9_data_normalization(self):
     np.random.seed(24)
-    mean = -700
-    std = 80
-    data = np.random.normal([mean, mean, mean, mean], std, size=(2000, 4))
+    mean = -80
+    std = 7
+    data = np.random.normal([mean, mean, mean, mean], std, size=(4000, 4))
     X = data[:, 0:2]
     Y = data[:, 2:4]
 
     with tf.Session() as sess:
       model = KernelMixtureNetwork("kmn_data_normalization_2", 2, 2, n_centers=5, x_noise_std=None, y_noise_std=None,
-                                    data_normalization=True, n_training_epochs=500, random_seed=22, keep_edges=False, train_scales=True, weight_normalization=True)
+                                    data_normalization=True, n_training_epochs=2000, random_seed=22, keep_edges=False,
+                                   train_scales=True, weight_normalization=True, init_scales=np.array([1.0]))
+
       model.fit(X, Y)
-      y_mean, y_std = sess.run([model.mean_y_sym, model.std_y_sym])
-      print(y_mean, y_std)
+      # y_mean, y_std = sess.run([model.mean_y_sym, model.std_y_sym])
+      # print(y_mean, y_std)
       cond_mean = model.mean_(Y)
       print(np.mean(cond_mean))
       mean_diff = np.abs(mean - np.mean(cond_mean))
@@ -599,10 +657,10 @@ class TestRegularization(unittest.TestCase):
 
       cond_cov = np.mean(model.covariance(Y), axis=0)
       print(cond_cov)
-      self.assertGreaterEqual(cond_cov[0][0], std * 0.7)
-      self.assertLessEqual(cond_cov[0][0], std * 1.3)
-      self.assertGreaterEqual(cond_cov[1][1], std * 0.7)
-      self.assertLessEqual(cond_cov[1][1], std * 1.3)
+      self.assertGreaterEqual(cond_cov[0][0], std**2 * 0.7)
+      self.assertLessEqual(cond_cov[0][0], std**2 * 1.3)
+      self.assertGreaterEqual(cond_cov[1][1], std**2 * 0.7)
+      self.assertLessEqual(cond_cov[1][1], std**2 * 1.3)
 
 
 #

cde/utils/tf_utils/layers.py
@@ -361,19 +361,20 @@ class VariableLayer(Layer):
     def get_output_for(self, input, **kwargs):
         return self.param
 
+
 class NormalizationLayer(Layer):
-    def __init__(self, incoming, feature_dim, **kwargs):
+    def __init__(self, incoming, feature_dim, name='', **kwargs):
         super(NormalizationLayer, self).__init__(incoming, **kwargs)
         self.mean = self.add_param(
-            tf.Variable(np.zeros(feature_dim, dtype=np.float32), name='data_norm_mean', trainable=False),
+            tf.Variable(np.zeros(feature_dim, dtype=np.float32), name=name+'_mean', trainable=False),
             (feature_dim,),
-            name='mean',
+            name=name+'mean',
             trainable=False
         )
         self.std = self.add_param(
-          tf.Variable(np.ones(feature_dim, dtype=np.float32), name='data_norm_mean', trainable=False),
+          tf.Variable(np.ones(feature_dim, dtype=np.float32), name=name+'_std', trainable=False),
           (feature_dim,),
-          name='mean',
+          name=name+'std',
           trainable=False
         )
 